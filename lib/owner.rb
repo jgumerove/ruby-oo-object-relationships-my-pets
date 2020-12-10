e8790e4899c9e0da28f9b9b66be1739e09760ef2
jgumerove <josh.gumerove@gmail.com>
2020-12-10 18:06:07 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Owner
-  # code goes here
+attr_reader :name, :species
+@@all = []
+def initialize (name)
+  @name = name
+  @species = "human"
+  @@all << self
+end
+def say_species
+"I am a #{self.species}."
+end
+def self.all
+  @@all
+end
+def self.count
+  self.all.count
+end
+def self.reset_all
+self.all.clear
+end
+def cats
+  Cat.all.find_all do |cat|
+  cat.owner == self
+  end
+end
+def dogs
+  Dog.all.find_all do |dog|
+  dog.owner == self
+  end
+end
+def buy_cat(name)
+  Cat.new(name, self)
+end
+def buy_dog(name)
+  Dog.new(name, self)
+end
+def walk_dogs
+  self.dogs.each do |dog|
+    dog.mood = "happy"
+  end
+end
+def feed_cats
+  self.cats.each do |cat|
+    cat.mood = "happy"
+  end
+end
 end
