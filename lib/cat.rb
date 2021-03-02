8fe39b39dff45917ddc8a9b3cd0c4c01108dc2ac
jgumerove <josh.gumerove@gmail.com>
2021-03-01 20:44:29 -0500

    commit tree c20513524f46950b95a9051ff15dc7f532c96b7e

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,14 +1,27 @@
+require 'pry'
+
 class Cat
-  attr_reader :name
-  attr_accessor :owner, :mood
-  @@all = []
-      def initialize (name, owner)
+    attr_reader :name
+    attr_accessor :owner, :mood
+
+    @@all = []
+ 
+
+    def initialize(name, owner)
         @name = name
         @owner = owner
         @mood = "nervous"
         @@all << self
-      end
-      def self.all
+    end
+
+    def self.all
         @@all
-      end
-  end
+    end
+
+    def self.count_cats
+        self.all.count
+    end
+    
+
+
+end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,48 +1,68 @@
-require 'pry'
 class Owner
-attr_reader :name, :species
-@@all = []
-def initialize (name)
-  @name = name
-  @species = "human"
-  @@all << self
-end
-def say_species
-"I am a #{self.species}."
-end
-def self.all
-  @@all
-end
-def self.count
-  self.all.count
-end
-def self.reset_all
-self.all.clear
-end
-def cats
-  Cat.all.find_all do |cat|
-  cat.owner == self
-  end
-end
-def dogs
-  Dog.all.find_all do |dog|
-  dog.owner == self
-  end
-end
-def buy_cat(name)
-  Cat.new(name, self)
-end
-def buy_dog(name)
-  Dog.new(name, self)
-end
-def walk_dogs
-  self.dogs.each do |dog|
-    dog.mood = "happy"
-  end
-end
-def feed_cats
-  self.cats.each do |cat|
-    cat.mood = "happy"
-  end
-end
+    attr_reader :name, :species
+    attr_accessor :dogs, :cats
+    
+    @@all = []
+    
+    def initialize(name)
+        @species = "human"
+        @name = name
+        @@all << self
+        @dogs = []
+        @cats = []
+    end
+
+    def say_species
+     "I am a #{self.species}."
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.count
+        self.all.count
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
+
+    def cats
+        Cat.all.select {|cat| cat.owner == self}
+    end
+
+    def dogs 
+        Dog.all.select{|dog| dog.owner == self}
+    end
+
+    def buy_cat(name)
+        @cats << cat = Cat.new(name, self)
+        cat.owner = self
+    end
+
+    def buy_dog(name)
+        @dogs << dog = Dog.new(name, self)
+        dog.owner = self
+    end
+
+    def walk_dogs
+        Dog.all.select{|dog| dog.mood = "happy" if dog.owner == self}
+    end
+
+    def feed_cats
+        Cat.all.select{|cat| cat.mood = "happy" if cat.owner == self}
+    end
+
+    def sell_pets
+        Dog.all.select{|dog| dog.mood = "nervous" if dog.owner == self} && Cat.all.select{|cat| cat.mood = "nervous" if cat.owner == self}
+        Dog.all.select{|dog| dog.owner = nil if dog.owner == self} && Cat.all.select{|cat| cat.owner = nil if cat.owner == self}          
+    end
+
+    def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    end
+        
+
+
 end
